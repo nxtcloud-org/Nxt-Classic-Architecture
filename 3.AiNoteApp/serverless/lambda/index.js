f82c0939b2c47496ebb5ea41d80bf73306e78b54
dxm-glen <114462252+dxm-glen@users.noreply.github.com>
2024-11-04 12:58:25 +0900

    Update index.js

--- 3.AiNoteApp/serverless/lambda/index.js
+++ 3.AiNoteApp/serverless/lambda/index.js
@@ -25,8 +25,7 @@ exports.handler = async (event) => {
         // OpenAI API 호출
         const completion = await openai.chat.completions.create({
             messages: [
-                { role: "system", content: "Your are expert in AWS, Tell me one AWS service names that I can learn additionally based on the data sent by the user.  in Korean" },
-                // { role: "system", content: "Your are expert in AWS, Tell me one AWS service names that I can learn additionally based on the data sent by the user. as one sentence in Korean" },
+                { role: "system", content: "You are an expert in AWS. Based on the data provided by the user, suggest one AWS service that the user can additionally learn. Ensure the response is at least three sentences long and in Korean." },
                 { role: "user", content: userMessage }
             ],
             model: "gpt-3.5-turbo",
